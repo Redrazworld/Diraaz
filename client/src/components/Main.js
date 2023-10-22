c8bc4c8d9c09c263c4269b786d4d2db57b97a176
Prashant Srivastav <srivastavp891@gmail.com>
2023-10-23 02:35:59 +0530

    basdic upload

---
+++ client/src/components/Main.js
@@ -0,0 +1,16 @@
+import Navbarcomponent from "./Navbarcomponent";
+import "../assets/styles/Main.css";
+function Main(){
+    return(
+
+<>
+<Navbarcomponent/>
+<div className="container-img">
+    <p className="paramain">Good things take time, Launching soon. Premium Hand-picked CTC & Leaf Teas.</p>
+</div>
+
+</>
+    );
+}
+
+export default Main;
